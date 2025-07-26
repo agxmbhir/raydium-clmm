fe8aec4b45913b663e994b5e3269ed2e724a03c3
agxmbhir <agambhir5656@gmail.com>
2025-07-26 14:28:53 -0700

    added lib exposure

--- client/src/instructions/mod.rs
+++ client/src/instructions/mod.rs
@@ -3,3 +3,10 @@ pub mod events_instructions_parse;
 pub mod rpc;
 pub mod token_instructions;
 pub mod utils;
+
+// Re-export commonly used functions from submodules
+pub use amm_instructions::*;
+pub use events_instructions_parse::*;
+pub use rpc::*;
+pub use token_instructions::*;
+pub use utils::*;
